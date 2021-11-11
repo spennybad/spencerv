1c9c9c7c408d90db335fd8d9c9123bc2eccaa26d
Spencercv7 <spencercv7@gmail.com>
2021-11-11 15:04:27 -0500

    Fixed Typos

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -21,7 +21,7 @@
             <div id="home_text">
                 <p><span>Hello!</span></p>
                 <p>My name is Spencer and I am a web developer.</p>
-                <p>I am a super passionate about what I do and spend my free time working on projects or learning new skills.</p>
+                <p>I am super passionate about what I do and spend my free time working on projects or learning new skills.</p>
                 <p>If you would like to work together please send me an email!</p>
                 <p style="font-size: calc(var(--font-size-p) + 1rem);">🔥</p>
             </div>
